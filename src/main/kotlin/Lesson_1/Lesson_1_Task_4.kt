93aca420d87cfeccc9b59041c4a2fdd6a731e0d2
Ando <dzansuandrej@gmail.com>
2025-06-11 16:21:02 +0300

    Variables commit

---
+++ src/main/kotlin/Lesson_1/Lesson_1_Task_4.kt
@@ -0,0 +1,19 @@
+package org.example.Lesson_1
+
+fun main() {
+
+    val lengh: Long = 40_868_600_000L
+    val age: Byte = 27
+    val partOfDay: Float = 0.075f
+    val seconds: Short = 6480
+    val partOfYear: Double = 2.0547945205479453E-4
+    val apogee: Int = 327000
+
+    println(lengh)
+    println(age)
+    println(partOfYear)
+    println(seconds)
+    println(partOfYear)
+    println(apogee)
+
+}
