307b1b524e0f65a6a69e36f93fc26cf7fc037c79
Ando <dzansuandrej@gmail.com>
2025-06-11 13:19:18 +0300

    KS-1-1 create2

---
+++ src/main/kotlin/Lesson_1/Lesson_1_Task_2.kt
@@ -0,0 +1,14 @@
+package org.example.Lesson_1
+
+fun main() {
+    val numberOfOrders: Int = 75
+    val thanksMessage: String = "Спасибо за покупку"
+    var numberOfEmployees: Int = 2000
+
+    println(numberOfOrders)
+    println(thanksMessage)
+//    println(numberOfEmployees)
+
+    numberOfEmployees = 1999
+    println(numberOfEmployees)
+}
