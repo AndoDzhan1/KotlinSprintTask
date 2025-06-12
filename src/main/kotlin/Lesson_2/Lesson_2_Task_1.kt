5214ee99616912a20c3c2f5c813af802de785fb4
Ando <dzansuandrej@gmail.com>
2025-06-12 14:14:44 +0300

    KS-2-1 corrected

--- src/main/kotlin/Lesson_2/Lesson_2_Task_1.kt
+++ src/main/kotlin/Lesson_2/Lesson_2_Task_1.kt
@@ -6,8 +6,9 @@ fun main() {
     val student2: Float = 4.0f
     val student3: Float = 3.0f
     val student4: Float = 5.0f
+    val countOfStudents: Int = 4
 
-    var averageScore = (student1 + student2 + student3 + student4) / 4
+    var averageScore = (student1 + student2 + student3 + student4) / countOfStudents
     println(averageScore)
 
 }
