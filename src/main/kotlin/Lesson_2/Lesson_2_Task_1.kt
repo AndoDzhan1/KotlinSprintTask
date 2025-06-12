cb255977ff65165799fd627203a928f2b88856b8
Ando <dzansuandrej@gmail.com>
2025-06-12 10:12:17 +0300

    KS-2-1

---
+++ src/main/kotlin/Lesson_2/Lesson_2_Task_1.kt
@@ -0,0 +1,13 @@
+package org.example.Lesson_2
+
+fun main() {
+
+    val student1: Float = 3.0f
+    val student2: Float = 4.0f
+    val student3: Float = 3.0f
+    val student4: Float = 5.0f
+
+    var averageScore = (student1 + student2 + student3 + student4) / 4
+    println(averageScore)
+
+}
