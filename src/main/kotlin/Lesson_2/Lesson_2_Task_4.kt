86ccc32f38c3ff5e057d918dc966f4585989cca7
Ando <dzansuandrej@gmail.com>
2025-06-12 15:19:52 +0300

    KS-2-4

---
+++ src/main/kotlin/Lesson_2/Lesson_2_Task_4.kt
@@ -0,0 +1,15 @@
+package org.example.Lesson_2
+
+fun main() {
+
+    val cristalOre: Int = 7
+    val ironOre: Int = 11
+
+    val buffEffect: Float = 0.2f
+
+    val cristalOreBuffEffect = cristalOre * buffEffect
+    val ironOreBuffEffect = ironOre * buffEffect
+
+    println(cristalOreBuffEffect)
+    println(ironOreBuffEffect)
+}
