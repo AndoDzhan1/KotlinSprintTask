5acde20b775ef16de3eff9b8c8ea48d49a4fbda6
Ando <dzansuandrej@gmail.com>
2025-06-12 12:08:34 +0300

    KS-1-3

--- src/main/kotlin/Lesson_1/Lesson_1_Task_3.kt
+++ src/main/kotlin/Lesson_1/Lesson_1_Task_3.kt
@@ -5,14 +5,14 @@ fun main() {
     val year = 1961
     var hour: String = "09"
     var minute: String = "07"
-    val colon: Char = ':'
 
-    println("Год: $year")
-    println("Время взлёта: $hour $colon $minute")
+    println(year)
+    println(hour)
+    println(minute)
 
     hour = "10"
     minute = "55"
 
-
-    println("Время посадки: $hour $colon $minute")
+    println(hour)
+    println(minute)
 }
