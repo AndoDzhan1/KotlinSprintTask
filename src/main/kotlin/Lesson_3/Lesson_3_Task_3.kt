d2f01de310422b4f3eb470022b2db9759ceae22e
Ando <dzansuandrej@gmail.com>
2025-06-14 05:49:09 +0300

    KS-3-3

--- src/main/kotlin/Lesson_3/Lesson_3_Task_3.kt
+++ src/main/kotlin/Lesson_3/Lesson_3_Task_3.kt
@@ -2,22 +2,19 @@ package org.example.Lesson_3
 
 fun main() {
 
-    val input: String = readln() //Для ввода
-    val otherNumber: Int = input.toInt() //переобразованна для ввода чисел
+    val input: String = readln()
+    val number: Int = input.toInt()
 
-    val number: String = """
-        1
-        2
-        3
-        4
-        5
-        6
-        7
-        8
-        9
-    """.trimIndent()
-
-    val calculate = otherNumber * number
-    println("$calculate ${otherNumber * number}")
+     println("""
+         $number * 1 = ${number * 1}
+         $number * 2 = ${number * 2}
+         $number * 3 = ${number * 3}
+         $number * 4 = ${number * 4}
+         $number * 5 = ${number * 5}
+         $number * 6 = ${number * 6}
+         $number * 7 = ${number * 7}
+         $number * 8 = ${number * 8}
+         $number * 9 = ${number * 9}
+     """.trimIndent())
 
 }
