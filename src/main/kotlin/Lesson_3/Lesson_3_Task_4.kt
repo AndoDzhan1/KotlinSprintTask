8bf395290dd2b6f5906c4a2fbc943125b359a7ce
Ando <dzansuandrej@gmail.com>
2025-06-14 21:21:17 +0300

    KS-3-4 corrected

--- src/main/kotlin/Lesson_3/Lesson_3_Task_4.kt
+++ src/main/kotlin/Lesson_3/Lesson_3_Task_4.kt
@@ -2,17 +2,17 @@ package org.example.Lesson_3
 
 fun main() {
 
-        var from: String = "E2"
-        var to: String = "E4"
-        var tripNumber: Int = 1
+    var from: String = "E2"
+    var to: String = "E4"
+    var tripNumber: Int = 1
 
-        println("Откуда:$from Куда:$to Номер хода:$tripNumber")
+    println("Откуда-Куда:$from-$to; Номер хода:$tripNumber")
 
     println("Следующий ход белых")
 
     from = "D2"
     to = "D3"
-    tripNumber = 1
+    tripNumber = ++tripNumber
 
-    println("Откуда:$from Куда:$to Номер хода:$tripNumber"  )
+    println("Откуда-Куда:$from-$to; Номер хода:$tripNumber")
 }
