3e08abb45ca2476826c632b8cf2d3f0944028ad3
Ando <dzansuandrej@gmail.com>
2025-06-15 09:24:06 +0300

    KS-2-4 fixed 3

--- src/main/kotlin/Lesson_3/Lesson_3_Task_4.kt
+++ src/main/kotlin/Lesson_3/Lesson_3_Task_4.kt
@@ -6,13 +6,13 @@ fun main() {
     var to: String = "E4"
     var tripNumber: Int = 1
 
-    println("$from-$to; $tripNumber")
+    println("$from-$to;$tripNumber")
 
     println("Следующий ход белых")
 
     from = "D2"
     to = "D3"
-    tripNumber = tripNumber++
+    tripNumber = tripNumber + 1
 
-    println("$from-$to; $tripNumber")
+    println("$from-$to;$tripNumber")
 }
