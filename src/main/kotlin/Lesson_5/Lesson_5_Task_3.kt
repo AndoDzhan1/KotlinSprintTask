aec758798d9d38cd490a8ec62f7b28edb1096e29
Ando <dzansuandrej@gmail.com>
2025-06-15 22:49:02 +0300

    KS-5-3

---
+++ src/main/kotlin/Lesson_5/Lesson_5_Task_3.kt
@@ -0,0 +1,21 @@
+package org.example.Lesson_5
+
+fun main() {
+
+    val inputNumber1 = readln().toInt()
+    println("Ваше первое число: $inputNumber1")
+    val inputNumber2 = readln().toInt()
+    println("Ваше второе число: $inputNumber2")
+
+    val scam = if (inputNumber1 == WINNING_NUMBERS && inputNumber2 == WINNING_NUMBERS_2) {
+        "Поздравляем! Вы выйграли главный приз!"
+    } else if (inputNumber1 == WINNING_NUMBERS || inputNumber2 == WINNING_NUMBERS_2) {
+        "Вы выйграли утешительный приз!"
+    } else {
+        "Неудача!"
+    }
+    println(scam)
+}
+
+const val WINNING_NUMBERS = 0
+const val WINNING_NUMBERS_2 = 42
