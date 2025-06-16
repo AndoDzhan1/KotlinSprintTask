0f525662a53f5058fe64f492e0de6fb5d5f4c65e
Ando <dzansuandrej@gmail.com>
2025-06-16 17:20:45 +0300

    KS-5-1 corrected

--- src/main/kotlin/Lesson_5/Lesson_5_Task_1.kt
+++ src/main/kotlin/Lesson_5/Lesson_5_Task_1.kt
@@ -3,15 +3,15 @@ package org.example.Lesson_5
 fun main() {
 
     println("Подтвердите что вы не робот 2 + 2 = ")
+    val num1 = 2
+    val num2 = 2
+    val sum = num1 + num2
     val varificate = readln().toInt()
 
-    if (varificate == ANSWER) {
+    if (varificate == sum) {
         println("Добро пожаловать!")
     } else {
         println("Доступ запрещён!")
     }
-
 }
-
-const val ANSWER = 4
 
