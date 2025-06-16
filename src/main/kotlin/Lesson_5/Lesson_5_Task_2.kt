e1a3bcdc8dd35755490f5022328e015386286d52
Ando <dzansuandrej@gmail.com>
2025-06-16 20:41:32 +0300

    KS-5-2 corrected

--- src/main/kotlin/Lesson_5/Lesson_5_Task_2.kt
+++ src/main/kotlin/Lesson_5/Lesson_5_Task_2.kt
@@ -8,7 +8,7 @@ fun main() {
 
     if (userAge <= AGE_OF_MAJORITY) {
         println("Показать экран со скрытым контентом, например, для алкогольной продукции")
-    } else if (userAge == 2009 || userAge == 2008) {
+    } else if (userAge >= TEENAGE_YEARS) {
         println("Показать экран с ограниченным контентом, например, для товаров с рейтингом 16+")
     } else {
         println("Вернуться на главный экран приложения, так как доступ ограничен для вашего возраста")
@@ -17,4 +17,4 @@ fun main() {
 }
 
 const val AGE_OF_MAJORITY = 2007
-const val RETIREMENT_AGE = 1950
+const val TEENAGE_YEARS = 2008
