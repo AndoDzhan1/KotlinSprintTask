0e988465f0c22ad112b680e265a8d6e386dfd50b
Ando <dzansuandrej@gmail.com>
2025-06-17 22:14:10 +0300

    KS-5-4 fixed

--- src/main/kotlin/Lesson_5/Lesson_5_Task_4.kt
+++ src/main/kotlin/Lesson_5/Lesson_5_Task_4.kt
@@ -5,19 +5,18 @@ fun main() {
     println("Введите ваше имя")
     val inputName = readln()
 
-    println("Введите пароль для доступа")
-    val inputPassword = readln()
+    if (inputName == NAME) {
+        println("Введите пароль для доступа")
+        val inputPassword = readln()
 
-    if (inputName == NAME && inputPassword == PASSWORD) {
-        println("Пользователь $inputName, вам разрешено входить на борт корабля \"Heart of Gold\"")
-    } else if (inputName == NAME) {
-        println("Неправильный логин или пароль")
-    } else if (inputPassword == PASSWORD) {
-        println("Неправильный логин или пароль")
+        if (inputPassword == PASSWORD) {
+            println("Пользователь $inputName, вам разрешено входить на борт корабля \"Heart of Gold\"")
+        } else {
+            println("Неправильный логин или пароль")
+        }
     } else {
         println("Этот пользователь не имеет доступ к управлению кораблем и не может осуществлять навигацию по вселенной")
     }
-
 }
 
 const val NAME = "Zaphod"
