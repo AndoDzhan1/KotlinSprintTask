9631596736cdbcc9efd0a1d0a654c6b4094ed4e5
Ando <dzansuandrej@gmail.com>
2025-06-23 13:27:07 +0300

    KS-6-1

---
+++ src/main/kotlin/Lesson_6/Lesson_6_Task_1.kt
@@ -0,0 +1,31 @@
+package Lesson_6
+
+fun main() {
+
+    var createLogin: String
+    var createPassword: String
+
+    println("Создание нового пользователя")
+
+    println("Cоздайте логин")
+    createLogin = readln()
+
+    println("Создайте пароль")
+    createPassword = readln()
+
+    println("Вход в систему")
+
+    while (true) {
+        println("Введите логин")
+        val inputLogin = readln()
+
+        println("Введите пароль")
+        val inputPassword = readln()
+
+        if (createLogin == inputLogin && createPassword == inputPassword) {
+            println("Добро пожаловать")
+        } else {
+            println("Неверный логин или пароль")
+        }
+    }
+}
