c448ad807f85e03d990df4f2d63884b33cd95d3a
Ando <dzansuandrej@gmail.com>
2025-06-24 11:54:26 +0300

    KS-6-2 corrected

--- src/main/kotlin/Lesson_6/Lesson_6_Task_2.kt
+++ src/main/kotlin/Lesson_6/Lesson_6_Task_2.kt
@@ -5,9 +5,6 @@ fun main() {
     println("Введите количество секунд")
     var countOfSeconds = readln().toInt()
 
-    while (countOfSeconds > 0) {
-        println("Прошло ${--countOfSeconds} секунд")
-        Thread.sleep(1000)
-    }
-
+    Thread.sleep(countOfSeconds * 1000L)
+    println("Прошло $countOfSeconds секунд")
 }
