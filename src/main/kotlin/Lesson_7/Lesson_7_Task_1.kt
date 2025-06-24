c0d90f6ed12a527200b7877f096bbf54eff531bd
Ando <dzansuandrej@gmail.com>
2025-06-24 21:54:53 +0300

    KS-7-1 corrected

--- src/main/kotlin/Lesson_7/Lesson_7_Task_1.kt
+++ src/main/kotlin/Lesson_7/Lesson_7_Task_1.kt
@@ -4,10 +4,13 @@ fun main() {
 
     val number = '0'..'9'
     val letters = 'a'..'z'
-    val allChar = (number + letters).toList()
 
     for (i in 1..6) {
-        val randomChar = allChar.random()
+        val randomChar = if (i %2 == 0) {
+            letters.random()
+        } else {
+            number.random()
+        }
         print(randomChar)
     }
 }
