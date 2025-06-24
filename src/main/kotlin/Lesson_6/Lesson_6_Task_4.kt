71b21ba6740a8e1f6c521d22e6557339f3716bd7
Ando <dzansuandrej@gmail.com>
2025-06-24 10:02:45 +0300

    KS-6-4

---
+++ src/main/kotlin/Lesson_6/Lesson_6_Task_4.kt
@@ -0,0 +1,19 @@
+package Lesson_6
+
+fun main(){
+
+    val answer = 6
+    var attemptsLeft = 5
+
+    while (attemptsLeft > 0){
+        println("Угадайте число от 1 до 9")
+        val guessNumber = readln().toInt()
+
+        if (guessNumber == answer) {
+            println("Это была великолепная игра!")
+        } else {
+            println("Неверно! \nОставшееся количество попыток ${attemptsLeft--}")
+        }
+    }
+    println("Было загадано число $answer")
+}
