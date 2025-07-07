b50a2e2451bf46e962a759dbec41fa7931a21df6
Ando <dzansuandrej@gmail.com>
2025-07-07 10:39:46 +0300

    KS-8-4 corrected

--- src/main/kotlin/Lesson_8/Lesson_8_Task_4.kt
+++ src/main/kotlin/Lesson_8/Lesson_8_Task_4.kt
@@ -4,20 +4,19 @@ fun main() {
 
     val ingredients: Array<String> = arrayOf("свинина", "лук", "перец", "соль", "киви")
 
-    println("Список ингредиентов:")
-    println(ingredients)
+    println("Список ингредиентов: ${ingredients.contentToString()}")
 
     println("Введите название ингредиента, который хотите заменить:")
     val changeIngredient = readln()
 
-    if (changeIngredient !in ingredients) {
-        println("Ингредиент \"$changeIngredient\" не найден в списке.")
-    } else {
+    val index = ingredients.indexOf(changeIngredient)
+
+    if (index != -1) {
         println("Введите новый ингредиент:")
         val newIngredient = readln()
-
-        val index = ingredients.indexOf(changeIngredient)
         ingredients[index] = newIngredient
-        println("Готово! Вы сохранили следующий список: $ingredients")
+        println("Готово! Вы сохранили следующий список: ${ingredients.contentToString()}")
+    } else {
+        println("Ингредиент не найден в списке.")
     }
 }
