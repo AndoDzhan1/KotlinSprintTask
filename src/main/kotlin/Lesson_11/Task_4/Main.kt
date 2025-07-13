fa2a1b98603ee8b0d8d006bc3c010dfd76b56e72
Ando <dzansuandrej@gmail.com>
2025-07-13 13:23:32 +0300

    KS-11-4

---
+++ src/main/kotlin/Lesson_11/Task_4/Main.kt
@@ -0,0 +1,19 @@
+package org.example.Lesson_11.Task_4
+
+class RecipeCategory(
+    val name: String,
+    val image: String,
+    val description: String,
+)
+
+class Recipe(
+    val name: String,
+    val image: String,
+    val ingredients: List<String>,
+    val portions: Int,
+)
+
+class Ingredient(
+    val nameOfIngrdients: List<String>,
+    val methodOfPreparation: String
+)
