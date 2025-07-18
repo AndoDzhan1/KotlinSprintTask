159e3658ca3787f0e430ccc5b59472ca45685667
AndoDzhan1 <dzansuandrej@gmail.com>
2025-07-18 15:59:29 +0300

    KS-12-1 corrected

--- src/main/kotlin/Lesson_12/Task_1/Main.kt
+++ src/main/kotlin/Lesson_12/Task_1/Main.kt
@@ -1,6 +1,10 @@
 package org.example.Lesson_12.Task_1
 
-class WeatherData(var daytimeTemperature: Int, var nighttimeTemperature: Int, var precepitation: Boolean = false) {
+class WeatherData() {
+
+    var daytimeTemperature = 0
+    var nighttimeTemperature = 0
+    var precepitation = false
 
     fun weatherInfo() {
         println("Днём: $daytimeTemperature")
@@ -10,13 +14,20 @@ class WeatherData(var daytimeTemperature: Int, var nighttimeTemperature: Int, va
 }
 
 fun main() {
-    val saturday = WeatherData(33, 25)
+    val saturday = WeatherData()
+    saturday.daytimeTemperature = 33
+    saturday.nighttimeTemperature = 25
+
     println("Погода на субботу")
     saturday.weatherInfo()
 
     println()
 
-    val sunday = WeatherData(35, 30, true)
+    val sunday = WeatherData()
+    sunday.daytimeTemperature = 35
+    sunday.nighttimeTemperature = 30
+    sunday.precepitation = true
+
     println("Погода на воскресенье")
     sunday.weatherInfo()
 }
