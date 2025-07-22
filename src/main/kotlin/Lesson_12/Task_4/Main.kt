5f4247f418e0e890f1d8ae9fce08c3d6e87b1891
AndoDzhan1 <dzansuandrej@gmail.com>
2025-07-22 13:43:23 +0300

    KS-12-4

---
+++ src/main/kotlin/Lesson_12/Task_4/Main.kt
@@ -0,0 +1,28 @@
+package org.example.Lesson_12.Task_4
+
+class DailyWeather(kelvinDay: Double, kelvinNight: Double, precipitation: Boolean) {
+
+    var dayTemperature: Int
+    var nightTemperature: Int
+    var hasPrecipitation: Boolean
+
+    init {
+        dayTemperature = (kelvinDay - 273.15).toInt()
+        nightTemperature = (kelvinDay - 273.15).toInt()
+        hasPrecipitation = precipitation
+
+        println("Днём: $dayTemperature")
+        println("Ночью: $nightTemperature")
+        println("Осадки: $hasPrecipitation")
+    }
+
+}
+
+fun main() {
+
+    val sunday = DailyWeather(
+        kelvinDay = 303.15,
+        kelvinNight = 288.15,
+        precipitation = true,
+    )
+}
