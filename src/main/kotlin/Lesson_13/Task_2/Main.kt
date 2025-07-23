3d9587021c61ac5019731bbd5e399329dfb5f12c
AndoDzhan1 <dzansuandrej@gmail.com>
2025-07-23 12:58:22 +0300

    KS-13-2

---
+++ src/main/kotlin/Lesson_13/Task_2/Main.kt
@@ -0,0 +1,31 @@
+package org.example.Lesson_13.Task_2
+
+class PhoneContact(
+    val name: String,
+    val phoneNumber: Long,
+    val company: String? = null,
+) {
+
+    fun contactInfo() {
+        val companyInfo = company ?: "Не указано"
+        println("Имя: $name \nНомер: $phoneNumber \nКомпания: $companyInfo")
+    }
+
+}
+
+fun main() {
+
+    val user = PhoneContact(
+        "Ростислав",
+        89123456789,
+        "Reddit"
+
+        )
+
+    if (user.company != null) {
+        println("Всё ок!")
+    }
+
+    user.contactInfo()
+
+}
