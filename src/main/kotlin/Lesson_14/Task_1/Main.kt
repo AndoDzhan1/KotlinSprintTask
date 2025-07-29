a93e8e75ddd4742bb5c37847eaa0335065fcfa64
AndoDzhan1 <169075537+AndoDzhan1@users.noreply.github.com>
2025-07-29 14:38:19 +0300

    Update Main.kt

--- src/main/kotlin/Lesson_14/Task_1/Main.kt
+++ src/main/kotlin/Lesson_14/Task_1/Main.kt
@@ -9,18 +9,18 @@ class Cargo(
     speed: Int,
     numberOfPassengers: Int,
     val carryingCapacity: Int,
-    val breakIce: Boolean,
 ) : Liner(speed, numberOfPassengers)
 
 class Icebreaker(
     speed: Int,
     numberOfPassengers: Int,
+    val breakIce: Boolean,
 ) : Liner(speed, numberOfPassengers)
 
 fun main() {
     val liner = Liner(24, 7600)
 
-    val cargo = Cargo(17, 12, 400000, true)
+    val cargo = Cargo(17, 12, 400000,)
 
-    val icebreaker = Icebreaker(22, 215)
+    val icebreaker = Icebreaker(22, 215, true)
 }
