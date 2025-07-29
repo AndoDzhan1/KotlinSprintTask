539bafb83075be9056e518e3c067749f99b47ffa
AndoDzhan1 <dzansuandrej@gmail.com>
2025-07-29 09:57:13 +0300

    KS-13-3 corrected

--- src/main/kotlin/Lesson_13/Task_3/Main.kt
+++ src/main/kotlin/Lesson_13/Task_3/Main.kt
@@ -1,18 +1,15 @@
 package org.example.Lesson_13.Task_3
 
 class Contact(
-
     val name: String,
     val phoneNumber: Long,
     val company: String? = null
-
 ) {
 
-    fun contactInfo() {
+    fun showContactInfo() {
         val companyInfo = company ?: "Не указано"
-        println("Имя: $name \nНомер: $phoneNumber \nКомпания: $companyInfo")
+        println("Имя: $name\nНомер: $phoneNumber\nКомпания: $companyInfo")
     }
-
 }
 
 fun main() {
@@ -21,12 +18,13 @@ fun main() {
         Contact("Артур", 89123456788, null),
         Contact("Гево", 89991234567, "null"),
         Contact("Билл", 87777777777, "Microsoft"),
-        Contact("Арсений", 89011236754, null)
+        Contact("Арсений", 89011236754, null),
+        Contact("Михаил", 89991324567, "Microsoft"),
     )
 
     println("Компании:")
     contacts
         .mapNotNull { it.company }
+        .toSet()
         .forEach { println(it) }
-
 }
