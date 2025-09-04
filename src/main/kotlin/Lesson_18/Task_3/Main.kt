36362f903371dcdcc71f55cfe92c2410614ab321
AndoDzhan1 <dzansuandrej@gmail.com>
2025-09-04 16:30:13 +0300

    KS-18-3

---
+++ src/main/kotlin/Lesson_18/Task_3/Main.kt
@@ -0,0 +1,35 @@
+package org.example.Lesson_18.Task_3
+
+abstract class Animal(val name: String) {
+    abstract fun eat()
+}
+
+class Fox(name: String) : Animal(name) {
+    override fun eat() {
+        println("$name -> ест ягоды")
+    }
+}
+
+class Dog(name: String) : Animal(name) {
+    override fun eat() {
+        println("$name -> ест кости")
+    }
+}
+
+class Cat(name: String) : Animal(name) {
+    override fun eat() {
+        println("$name -> ест рыбу")
+    }
+}
+
+fun main() {
+    val animals: List<Animal> = listOf(
+        Fox("Лиса"),
+        Dog("Собака"),
+        Cat("Кошка"),
+    )
+
+    for (animal in animals) {
+        animal.eat()
+    }
+}
