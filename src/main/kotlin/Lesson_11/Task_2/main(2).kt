d339d0da82e86f7b598ee341f2fcb034cdca3e3d
AndoDzhan1 <dzansuandrej@gmail.com>
2025-09-06 09:20:08 +0300

    KS-11-2 fixed

--- src/main/kotlin/Lesson_11/Task_2/main(2).kt
+++ src/main/kotlin/Lesson_11/Task_2/main(2).kt
@@ -1,51 +1,45 @@
 package org.example.Lesson_11.Task_2
 
-class User2 (
+class User2(
     val id: Int,
     val login: String,
     var password: String,
     val email: String,
     var bio: String = "",
-)
+) {
+
+    fun userInfo() {
+        println("Ваша информация:")
+        println("Логин: $login")
+        println("Email: $email")
+        println("Пароль: $password")
+    }
+
+    fun changePassword() {
+        println("Введите ваш текущий пароль:")
+        val inputPassword = readln()
+
+        if (inputPassword == password) {
+            println("Введите новый пароль:")
+            password = readln()
+            println("Ваш новый пароль: $password")
+        } else {
+            println("Пароль введён неверно")
+        }
+    }
+}
 
 fun main() {
-
     val user1 = User2(
         id = 1,
         login = "Ando1",
         password = "123",
         email = "ando123",
         bio = "",
-
-        )
+    )
 
     println(user1.bio)
-    userInfo(user1)
-    changePassword(user1)
-    userInfo(user1)
-
-}
-
-fun userInfo(user1: User2) {
-
-    println("Ваша информация:")
-    println(user1.login)
-    println(user1.email)
-    println(user1.password)
-
-}
-
-fun changePassword(user1: User2) {
-
-    println("Введите ваш текущий пароль:")
-    val inputPassword = readln()
-
-    if (inputPassword == user1.password) {
-        println("Введите новый пароль")
-        user1.password = readln()
-        println("Ваш новый пароль: ${user1.password}")
-    } else {
-        println("Пароль введён неверно")
-    }
-
+    user1.userInfo()
+    user1.changePassword()
+    user1.userInfo()
 }
