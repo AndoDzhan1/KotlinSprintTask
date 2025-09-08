f1e33485dc8d1d3b56b6432f21c51217290ff897
AndoDzhan1 <dzansuandrej@gmail.com>
2025-09-08 19:49:46 +0300

    KS-19-1 fixed

--- src/main/kotlin/Lesson_19/Task_1/Main.kt
+++ src/main/kotlin/Lesson_19/Task_1/Main.kt
@@ -1,14 +1,15 @@
 package org.example.Lesson_19.Task_1
 
 fun main() {
-    println("Вы можете добавить следующих рыб")
-    val aquarium = Fish.values().map{it.title}
-    println(aquarium)
+    println("Вы можете добавить следующих рыб:")
+
+    val aquarium = Fish.entries
+    aquarium.forEach { println(it.title) }
 }
 
 enum class Fish(val title: String) {
     GUPPY("Гуппи"),
     ANGELFISH("Скалярия"),
-    GOLDEFISH("Золотая рыбка"),
+    GOLDFISH("Золотая рыбка"),
     FIGHTINGFISH("Петушок"),
 }
