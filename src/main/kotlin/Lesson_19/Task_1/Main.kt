5d590f86d37d4a2276e7b46a37d0217ff6c09bc1
AndoDzhan1 <dzansuandrej@gmail.com>
2025-09-08 11:18:54 +0300

    KS-19-1 corrected

--- src/main/kotlin/Lesson_19/Task_1/Main.kt
+++ src/main/kotlin/Lesson_19/Task_1/Main.kt
@@ -2,7 +2,7 @@ package org.example.Lesson_19.Task_1
 
 fun main() {
     println("Вы можете добавить следующих рыб")
-    val aquarium = listOf(Fish.GUPPY.title, Fish.ANGELFISH.title, Fish.GOLDEFISH.title, Fish.FIGHTINGFISH.title)
+    val aquarium = Fish.values().map{it.title}
     println(aquarium)
 }
 
